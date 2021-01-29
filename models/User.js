932bcbbeaf4df8c3d2be6f5d507481eb6faabdf3
Bunks <busk_1@yahoo.com>
2021-01-29 13:50:15 -0500

    User model complete

---
+++ models/User.js
@@ -0,0 +1,13 @@
+module.exports = function (sequelize, DataTypes) {
+  const User = sequelize.define("User", {
+    userName: DataTypes.STRING,
+  });
+
+  User.associate = function (models) {
+    // Associating User with Drawings.
+    // When a user is deleted, delete all their drawings.
+    Author.hasMany(models.Drawings, {
+      onDelete: "cascade",
+    });
+  };
+};
